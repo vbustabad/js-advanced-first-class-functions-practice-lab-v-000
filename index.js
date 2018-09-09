4b46b26287f1b4f96b6eabc2694722dea877679c
Veronica Bustabad <vbustabad@gmail.com>
2018-09-09 01:01:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const logDriversByHometown = function(drivers, hometown) {
   });
 };
 
-const comparator = function (a, b) {
-  return a.localeCompare(b);
+const sort_by_revenue = function (num1, num2) {
+  return num1 - num2;
 };
 
 const driversByRevenue = function(drivers) {
-  return drivers.sort(comparator);
+  return drivers.sort(sort_by_revenue);
 };
