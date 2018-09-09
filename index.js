a630e53f56db98ade8a03a6d7a80d193dd6d969e
Veronica Bustabad <vbustabad@gmail.com>
2018-09-09 01:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ const driversByName = function(drivers) {
 const totalRevenue = function(drivers) {
   return drivers.reduce(function(agg, el, i, arr) {
     return agg + el.revenue;
-  });
+  }, 0);
 };
