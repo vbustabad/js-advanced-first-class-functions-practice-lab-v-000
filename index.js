cddddea13793bbe74e97d158f418797ae9526d8f
Veronica Bustabad <vbustabad@gmail.com>
2018-09-09 00:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers) {
+  return drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, hometown) {
+  return drivers.forEach(function(driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+
+// const driversByRevenue = function(drivers) {
+//   return drivers.sort(function (a, b) {
+//     return a.localeCompare(b);
+//   });
+// };
